4279cf7f3cc5a787906fe338031c258ba0a310ef
Sibusiso <itsbudamain.05@gmail.com>
2021-02-03 22:10:46 +0200

    saving before I do something risky

--- src/app.js
+++ src/app.js
@@ -1,25 +1,38 @@
-const path = require("path")
-const express = require("express")
+const path = require("path");
+const express = require("express");
 
-console.log(__dirname)
+// define paths for express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname,"../templates");
 
-const joinedPath = path.join(__dirname, "..")
-console.log(joinedPath)
+const app = express();
 
-const app = express()
+app.set("view engine","hbs");
+app.set("views", viewPath);
+app.use(express.static(publicDirectoryPath));
 
-app.get("", (req, res) => {
-	res.send("Hello express")
+app.get("/", (req, res) => {
+	res.render("index", {
+		title: "Weather",
+		name: "Andrew mead"
+	});
 });
 
-app.get("/help", (req, res) => {
-	res.send("Help page")
+app.get("/About", (req,res)=> {
+
+	res.render("About", {
+		title: "About",
+		name: "Psych"
+	})
 })
 
-app.get("/about", (req,res) => {
-	res.send("<h1>About</h1>")
-})
+app.get("/Help", (req,res) => {
 
+	res.render("Help", {
+		title: "Help",
+		helpText: "Ooops... something went wrong :("
+	})
+})
 app.get("/weather", (req,res) => {
 	res.send({
 		current: {
@@ -30,8 +43,8 @@ app.get("/weather", (req,res) => {
 	})
 })
 
-const PORT = 3000
+const PORT = 3000;
 app.listen(PORT, () => {
 	// everything here shows up in the terminal
-	console.log(`Server up and running on port:[${PORT}]`)
+	console.log(`Server up and running on port:[${PORT}]`);
 })
